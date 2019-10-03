82bac2a9af920f15a4f8bf8c4c101558e519a586
bmh5582 <bmh5582@I4-CC-208-W07.AD.PSU.EDU>
2019-10-03 17:49:16 -0400

    Added Packages and Classes

---
+++ JavaApplication6/src/Database/Accounts/HospitalEmployee.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database.Accounts;
+
+/**
+ *
+ * @author bmh5582
+ */
+public class HospitalEmployee {
+    
+}
