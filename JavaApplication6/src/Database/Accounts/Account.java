8365a8665a0d86362685d3805e55158145d91734
Brandon Hillenbrand <bmh722@verizon.net>
2019-10-06 19:19:33 -0400

    Made Account a superclass, added some testing methods, cleaned stuff up a bit.

--- JavaApplication6/src/Database/Accounts/Account.java
+++ JavaApplication6/src/Database/Accounts/Account.java
@@ -1,26 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Database.Accounts;
 
-/**
- *
- * @author bmh5582
- */
 public class Account {
     
     private String username;
     private String password;
     private String type;
     
+    public Account(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    
     public Account(String username, String password, String type){
         this.username = username;
         this.password = password;
         this.type = type;
     }
-    
+    //setters
     public void setUsername(String username){
         this.username = username;
     }
@@ -30,6 +26,7 @@ public class Account {
     public void setType(String type){
         this.type = type;
     }
+    //getters1
     public String getUsername(){
         return this.username;
     }
@@ -39,5 +36,58 @@ public class Account {
     public String getType(){
         return this.type;
     }
+    //toString
+    @Override
+    public String toString(){
+        return "Username: " + this.username + "\nPassword: [REDACTED]" + "\nType: " + this.type; 
+    }
     
+    public Account createTestAccount(){
+        String Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        
+        String testUsername;
+        String testPassword;
+        String testType;
+        
+        
+        //random first name
+        StringBuilder sbu = new StringBuilder(10);
+        for(int i = 0; i < 10; i++){
+            int index = (int)((int)Alpha.length() * Math.random());
+            sbu.append(Alpha.charAt(index));
+        }
+        testUsername = sbu.toString();
+        
+        //random last name
+        StringBuilder sbp = new StringBuilder(10);
+        for(int i = 0; i < 10; i++){
+            int index = (int)((int)Alpha.length() * Math.random());
+            sbp.append(Alpha.charAt(index));
+        }
+        testPassword = sbp.toString();
+        
+        String[] typeList = {"Patient", "Hospital Employee", "Medical Professional", "Hospital Administrator"};
+        testType = typeList[(int)(Math.random() * 5)];
+        
+        return new Account(testUsername, testPassword, testType);
+    }
+    
+    public void signIn(String checkUsername, String checkPassword){
+        if(this.username.equals(checkUsername) && this.password.equals(checkPassword)){
+            System.out.println("Welcome " + this.username + "!");
+        }
+        else if(this.username.equals(checkUsername) && this.password != checkPassword){
+            System.out.println("Password is incorrect, please try again.");
+        }
+        else if(this.username != checkUsername && (this.password.equals(checkPassword) || this.password != checkPassword)){
+            System.out.println("Username does not exist, please try again or create an account");
+        }
+        else if(this.username == null && this.password == null){
+            System.out.println("Please create an account to sign in.");
+        }
+    }
+    
+    public void signOut(){
+        System.out.println("Goodbye, " + this.username + "!");
+    }
 }
