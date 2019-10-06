2aa179bf5f414f7d39933ae15bf21bc74c73d22e
Sam Straub <49694280+samstraub@users.noreply.github.com>
2019-10-06 16:52:07 -0400

    Update Patient.java

--- JavaApplication6/src/Database/Accounts/Patient.java
+++ JavaApplication6/src/Database/Accounts/Patient.java
@@ -5,18 +5,20 @@
  */
 package Database.Accounts;
 
-/**
- *
- * @author bmh5582
- */
+import java.util.ArrayList;
+
 public class Patient {
     
     private Account account;
     private PersonalInformation personalInformation;
+    private InsuranceInformation insuranceInformation;
+    private ArrayList<MedicalRecord> medicalRecords;
     
     public Patient(Account account){
         this.account = account;
         this.personalInformation = null;
+        this.insuranceInformation = null;
+        this.medicalRecords = new ArrayList<MedicalRecord>();
     }
     
     public void setAccount(Account account){
@@ -25,11 +27,31 @@ public class Patient {
     public void setPersonalInformation(PersonalInformation personalInformation){
         this.personalInformation = personalInformation;
     }
+    public void setInsuranceInformation(InsuranceInformation insuranceInformation){
+        this.insuranceInformation = insuranceInformation;
+    }
+    public void setMedicalRecords(ArrayList<MedicalRecord> medicalRecords){
+        this.medicalRecords = medicalRecords;
+    }
+    
     public Account getAccount(){
         return this.account;
     }
     public PersonalInformation getPersonalInformation(){
         return this.personalInformation;
+    }
+    public InsuranceInformation getInsuranceInformation(){
+        return this.insuranceInformation;
+    }
+    public ArrayList<MedicalRecord> getMedicalRecords(){
+        return this.medicalRecords;
+    }
+    
+    public void addMedicalRecord(MedicalRecord record){
+        this.medicalRecords.add(record);
+    }
+    public void removeMedicalRecord(MedicalRecord record){
+        this.medicalRecords.remove(record);
     }
     
 }
