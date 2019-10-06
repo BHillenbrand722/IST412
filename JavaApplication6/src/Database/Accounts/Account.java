4aa9aa97460e043ea59cc358eafafd4f79eba4d6
Sam Straub <49694280+samstraub@users.noreply.github.com>
2019-10-06 16:21:00 -0400

    Update Account.java

--- JavaApplication6/src/Database/Accounts/Account.java
+++ JavaApplication6/src/Database/Accounts/Account.java
@@ -11,4 +11,33 @@ package Database.Accounts;
  */
 public class Account {
     
+    private String username;
+    private String password;
+    private String type;
+    
+    public Account(String username, String password, String type){
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
+    
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public String getType(){
+        return this.type;
+    }
+    
 }
