b5de139d01be89af09503b721cb7a45ee2a48326
Sam Straub <49694280+samstraub@users.noreply.github.com>
2019-10-06 17:08:00 -0400

    Update HospitalAdministrator.java

--- JavaApplication6/src/Database/Accounts/HospitalAdministrator.java
+++ JavaApplication6/src/Database/Accounts/HospitalAdministrator.java
@@ -11,4 +11,34 @@ package Database.Accounts;
  */
 public class HospitalAdministrator {
     
+    private Account account;
+    private PersonalInformation personalInformation;
+    private String title;
+    
+    public MedicalProfessional(Account account){
+        this.account = account;
+        this.personalInformation = null;
+        this.title = "";
+    }
+    
+    public void setAccount(Account account){
+        this.account = account;
+    }
+    public void setPersonalInformation(PersonalInformation info){
+        this.personalInformation = info;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    public Account getAccount(){
+        return this.account;
+    }
+    public PersonalInformation getPersonalInformation(){
+        return this.personalInformation;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    
 }
