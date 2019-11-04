455f23541e947d955a67511ae4725c15fa07d692
AbstractFeel <55677649+AbstractFeel@users.noreply.github.com>
2019-11-03 22:43:11 -0500

    Add files via upload
    Did a lot, FXML UIs and is runnable now!

--- JavaApplication6/src/Application/AlphaCare.java
+++ JavaApplication6/src/Application/AlphaCare.java
@@ -5,18 +5,50 @@
  */
 package Application;
 
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author bmh5582
  */
-public class AlphaCare {
+public class AlphaCare extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+              
+             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Application/loginView.fxml"));
+             MainController controller = new MainController();
+             loader.setController(controller);
+             Parent root = loader.load();
+             primaryStage.setScene(new Scene(root, 600, 400));
+             primaryStage.show();
+        }
+        catch(IOException e){
+            System.out.println("Failed to start application: IOException");
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        MainController controller = new MainController();
+        launch(args);
+        //MainController controller = new MainController();
     }
-
+    
 }
--- JavaApplication6/src/Application/MainController.java
+++ JavaApplication6/src/Application/MainController.java
@@ -6,7 +6,19 @@
 package Application;
 
 import Database.MainModel;
-import View.MainView;
+import View.MainDashboardViewController;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  *
@@ -14,14 +26,19 @@ import View.MainView;
  */
 public class MainController {
 
-  MainModel model;
-  MainView  view;
+  public MainModel model;
+  
+  Stage currentStage;
+  
+ //Login FX Ids 
+  @FXML TextField usernameField;
+  @FXML PasswordField passwordField;
+  @FXML public Button  loginBtn;
+  @FXML Button  createBtn;
+  @FXML Text loginerrortext;
 
   public MainController(){
-
     model = new MainModel();
-    view = new MainView();
-
   }
 
   //Returns the main model object.
@@ -29,9 +46,53 @@ public class MainController {
     return model;
   }
 
-  //Returns the main view class.
-  public MainView getMainView(){
-    return view;
+
+  
+  
+  @FXML public void switchToCreate(ActionEvent event) throws IOException{
+      
+     
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Application/createAccountView.fxml"));
+        CreateAccountController controller = new CreateAccountController(this);
+        loader.setController(controller);
+        Parent root = loader.load();
+        createBtn.getScene().setRoot(root);
+        
+  }
+  
+  @FXML public void moveOnToDashboard() throws IOException{
+        Stage mainStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/mainDashboardView.fxml"));
+        View.MainDashboardViewController controller = new View.MainDashboardViewController(this);
+        loader.setController(controller);
+        
+        Parent root = loader.load();
+        mainStage.setScene(new Scene(root, 1200, 900));
+        mainStage.show();
+      
+        
+  }
+  
+  
+  public void autheticateCredentials(){
+      String authUsername = usernameField.getText();
+      String authPassword = passwordField.getText();
+      System.out.println(model.authenticatePatient(authUsername, authPassword));
+      if(model.authenticatePatient(authUsername, authPassword).equals("Login Successful")){
+          try{
+            model.setCurrentUserPatient();
+            this.moveOnToDashboard();
+            loginerrortext.setText("Login Successful!");
+            loginerrortext.setVisible(true);
+          }
+          catch(IOException e){
+              
+          }
+      }
+      else{
+          loginerrortext.setText("Invalid Login");
+          loginerrortext.setVisible(true);
+      }
   }
 
 }
