7e6f99f67064b9bacb85b68780a479e49cf5ab56
AbstractFeel <55677649+AbstractFeel@users.noreply.github.com>
2019-11-03 22:46:49 -0500

    Add files via upload
    Most amount of changes.

--- JavaApplication6/src/Database/MainModel.java
+++ JavaApplication6/src/Database/MainModel.java
@@ -5,6 +5,9 @@
  */
 package Database;
 
+import Database.Accounts.Patient;
+import java.util.ArrayList;
+
 /**
  *
  * @author bmh5582
@@ -12,10 +15,56 @@ package Database;
 public class MainModel {
 
   //Will create list or sometime of storage of accounts here.
+  public ArrayList<Database.Accounts.Patient> patientList;
+  
+  //Singleton Design Pattern Part 1
+  private static ArrayList<Database.Accounts.HospitalAdministrator> hospitalAdministratorList;
+  
+  public Database.Accounts.Patient currentUserPatient;
+  public int throwaway;
   
   public MainModel(){
-
+      
   }
-
-
+  
+  public ArrayList<Database.Accounts.Patient> getPatientList(){
+      if (patientList==null) 
+            patientList = new ArrayList<Database.Accounts.Patient>(); 
+      return patientList; 
+  }
+  
+  public String authenticatePatient(String un, String pw){
+      
+      for(int i = 0; i< getPatientList().size(); i++){
+        if(getPatientList().get(i).getAccount().getUsername().equals(un)){
+            if(getPatientList().get(i).getAccount().getPassword().equals(pw)){
+                throwaway = i;
+                return "Login Successful";
+            }
+        }
+      }
+      return "Login Invalid";
+  }
+  
+  public void addNewPatient(String newU, String newP, String type){
+      int accountnum = (int) (Math.random() * 100000);
+      Database.Accounts.Account newaccount = new Database.Accounts.Account(newU, newP, type);
+      Database.Accounts.Patient newpatient = new Database.Accounts.Patient(newaccount, accountnum);
+      getPatientList().add(newpatient);
+      
+  }
+  
+  public void setCurrentUserPatient(){
+      currentUserPatient = getPatientList().get(throwaway);
+  }
+  
+  public Patient getCurrentUserPatient(){
+      return currentUserPatient;
+  }
+  
+  //Singleton Design Pattern Part 2
+  public static ArrayList<Database.Accounts.HospitalAdministrator> getHospitalAdministratorList(){
+      return  hospitalAdministratorList;
+  }
+          
 }
